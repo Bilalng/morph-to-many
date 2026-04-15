83ad2923d2a1a3d2f1c355595df3cc9b6b5bdb4a
Bilalng <you@example.com>
2026-04-15 15:24:27 +0300

    fixed spelling mistake

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Commet extends Model
+class Comment extends Model
 {
     protected $table = 'comments';
     protected $fillable = [
