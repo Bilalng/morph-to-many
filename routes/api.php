d1ad385c2672e5d840602085aabe5351d0a671a8
Bilalng <you@example.com>
2026-04-15 16:56:06 +0300

    Add the endpoint and PEST test

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\VideoController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::apiResource('videos', VideoController::class)->except(['update']);
+    Route::apiResource('posts', PostController::class)->except(['update']);
+    Route::apiResource('comments', CommentController::class)->except(['update']);
+});
